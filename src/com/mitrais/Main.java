861160f7f2121822bcdfa51650aacb7d639d9939
Alvian Nur Ramadhani <alviannur.ramadhani@mitrais.com>
2022-03-04 19:35:31 +0700

    cleaning main

--- src/com/mitrais/Main.java
+++ src/com/mitrais/Main.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Stream part
+
+       // Optional part
        OptionalExample optionalExample = new OptionalExample();
        String s1Value=new String("Hello ");
        String value2=null;
-
+       // of() could throw NPE
        Optional<String> s1 = Optional.of(s1Value);
+       // ofNullable() can handle NPE
        Optional<String> s2 = Optional.ofNullable(value2);
 
        System.out.println(optionalExample.concat(s1,s2));
